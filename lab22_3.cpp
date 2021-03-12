164bd0c6fba067647e04374c3744e3cb252a6d0b
Autoz <kuro123520@hotmail.com>
2021-03-12 15:46:40 +0700

    lab22_3 - success

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
-//Write function count() here.
+int count(int d[],unsigned int a){
+	set<int> myCount;
+	for(unsigned int i = 0 ; i < a ; i++){
+		myCount.insert(d[i]);
+	}
+	return myCount.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
